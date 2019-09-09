c2da175fae65b9466ac6244c72720884e899a362
Student <student@pop-os.localdomain>
2019-09-10 11:00:51 +1200

    added more to beer listings

--- client/components/BeerList.jsx
+++ client/components/BeerList.jsx
@@ -1,20 +1,25 @@
-import React, {Component, Fragment} from 'react'
+import React, { Component, Fragment } from 'react'
 import data from '../../data/beers'
 import { connect } from 'react-redux';
 
-class BeerList extends Component{
-    render(){
-        return(
-        <Fragment>
-        <p>Welcome! Please select from our delicous selection and don't hesitate to let us know if we can anser any o your questions.</p>
-        <ul>
-             {data.beers.map(beer => <li key={beer.id}>{beer.name}</li>)}
-        </ul>
-       
-        </Fragment>
-    )
+class BeerList extends Component {
+    render() {
+        return (
+            <Fragment>
+                <p>Welcome! Please select from our delicous selection and don't hesitate to let us know if we can anser any o your questions.</p>
+                <ul>
+                    {data.beers.map(beer => <li key={beer.id}>{beer.name}
+                        <ul>
+                            <li> {beer.brewery}</li>
+                            <li>{beer.style}</li>
+                        </ul>
+                    </li>)}
+                </ul>
+
+            </Fragment>
+        )
     }
-    
+
 }
 
 export default connect()(BeerList)
